5b01c27543fbd3d82125bda3f35ab5168083be42
dee25092005 <146152064+dee25092005@users.noreply.github.com>
2024-08-22 19:40:33 +0700

    commit

---
+++ src/components/LoginComponent.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="hold-transition login-page">
+        <div class="login-box">
+  <!-- /.login-logo -->
+  <div class="card card-outline card-primary">
+    <div class="card-body">
+      <p class="login-box-msg">Sign in to start your session</p>
+      <h2 class="text-center text-primary"><b>ເຂົ້າສູ່ລະບົບ</b></h2>
+      <form>
+        <div class="input-group mb-3">
+          <input type="text" class="form-control" placeholder="ເບີໂທ">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-phone"></span>
+            </div>
+          </div>
+        </div>
+        <div class="input-group mb-3">
+          <input type="password" class="form-control" placeholder="********">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-key"></span>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-8">
+            <div class="icheck-primary">
+              <input type="checkbox" id="remember">
+              <label for="remember">
+                Remember Me
+              </label>
+            </div>
+          </div>
+          <!-- /.col -->
+          <div class="col-4">
+            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+          </div>
+          <!-- /.col -->
+        </div>
+      </form>
+
+      
+    </div>
+    <!-- /.card-body -->
+  </div>
+  <!-- /.card -->
+</div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginComponent'
+}
+</script>
